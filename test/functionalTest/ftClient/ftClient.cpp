66316db22499237e4de94cd11a8b751dcb10e3ab
Ken Zangelin <kzangeli@gmail.com>
2024-04-11 12:46:26 +0200

    Fixed a copyright header

--- test/functionalTest/ftClient/ftClient.cpp
+++ test/functionalTest/ftClient/ftClient.cpp
@@ -5,7 +5,7 @@
 * This file is part of Orion-LD Context Broker.
 *
 * Orion-LD Context Broker is free software: you can redistribute it and/or
-* modify it under the terms of the GNU Affero General Public
+* modify it under the terms of the GNU Affero General Public License as
 * published by the Free Software Foundation, either version 3 of the
 * License, or (at your option) any later version.
 *
